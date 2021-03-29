5746ed6b0544ed1b44eea12e6d5e8f80b04798e4
toadereandreas <57907218+toadereandreas@users.noreply.github.com>
2021-03-29 21:37:49 +0300

    Added all EC and BVA tests

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -7,6 +7,7 @@ import repository.StudentRepository;
 import repository.StudentXMLRepository;
 import service.Service;
 import validation.StudentValidator;
+import validation.ValidationException;
 import validation.Validator;
 
 public class AppTest {
@@ -41,4 +42,262 @@ public class AppTest {
 
     }
 
+    @Test
+    public void tc_1(){
+
+        Validator<Student> studentValidator = new StudentValidator();
+
+        studentXMLRepository = new StudentXMLRepository(studentValidator, "studenti.xml");
+
+        studentXMLRepository.save(new Student("1","Wayne",937));
+
+        assertEquals("1",studentXMLRepository.findOne("1").getID());
+
+    }
+
+    @Test
+    public void tc_2(){
+
+        Validator<Student> studentValidator = new StudentValidator();
+
+        studentXMLRepository = new StudentXMLRepository(studentValidator, "studenti.xml");
+
+        studentXMLRepository.save(new Student("100","Wayne",937));
+        studentXMLRepository.save(new Student("100","Bruce",937));
+
+        assertEquals("Wayne",studentXMLRepository.findOne("100").getNume());
+    }
+
+    @Test
+    public void tc_3(){
+
+        Validator<Student> studentValidator = new StudentValidator();
+
+        studentXMLRepository = new StudentXMLRepository(studentValidator, "studenti.xml");
+
+        assertEquals(null,studentXMLRepository.save(new Student("","Wayne",937)));
+    }
+
+    @Test
+    public void tc_4(){
+
+        Validator<Student> studentValidator = new StudentValidator();
+
+        studentXMLRepository = new StudentXMLRepository(studentValidator, "studenti.xml");
+
+        assertEquals(null,studentXMLRepository.save(new Student("111","",937)));
+    }
+
+    @Test
+    public void tc_5(){
+
+        Validator<Student> studentValidator = new StudentValidator();
+
+        studentXMLRepository = new StudentXMLRepository(studentValidator, "studenti.xml");
+
+        assertEquals(null,studentXMLRepository.save(new Student("111","",-1)));
+    }
+
+    @Test
+    public void tc_6(){
+
+        Validator<Student> studentValidator = new StudentValidator();
+
+        studentXMLRepository = new StudentXMLRepository(studentValidator, "studenti.xml");
+
+        assertEquals(null,studentXMLRepository.save(new Student("111","",-1)));
+    }
+
+
+    @Test
+    public void bva_2(){
+
+        Validator<Student> studentValidator = new StudentValidator();
+
+        studentXMLRepository = new StudentXMLRepository(studentValidator, "studenti.xml");
+
+        Student student = new Student(" ","wayne",937);
+
+        assertEquals(student,studentXMLRepository.save(student));
+    }
+
+    @Test
+    public void bva_3(){
+
+        Validator<Student> studentValidator = new StudentValidator();
+
+        studentXMLRepository = new StudentXMLRepository(studentValidator, "studenti.xml");
+
+        Student student = new Student("a","wayne",937);
+
+        assertEquals(student,studentXMLRepository.save(student));
+    }
+
+    @Test
+    public void bva_4(){
+
+        Validator<Student> studentValidator = new StudentValidator();
+
+        studentXMLRepository = new StudentXMLRepository(studentValidator, "studenti.xml");
+
+        Student student = new Student("111","wayne",937);
+
+        assertEquals(student,studentXMLRepository.save(student));
+    }
+
+    @Test
+    public void bva_5(){
+
+        Validator<Student> studentValidator = new StudentValidator();
+
+        studentXMLRepository = new StudentXMLRepository(studentValidator, "studenti.xml");
+
+        Student student = new Student("a1b2c3","wayne",937);
+
+        assertEquals(student,studentXMLRepository.save(student));
+    }
+
+    @Test
+    public void bva_6(){
+
+        Validator<Student> studentValidator = new StudentValidator();
+
+        studentXMLRepository = new StudentXMLRepository(studentValidator, "studenti.xml");
+
+        Student student = new Student("z","wayne",937);
+
+        assertEquals(student,studentXMLRepository.save(student));
+    }
+
+    @Test
+    public void bva_7(){
+
+        Validator<Student> studentValidator = new StudentValidator();
+
+        studentXMLRepository = new StudentXMLRepository(studentValidator, "studenti.xml");
+
+        Student student = new Student("id1","",937);
+
+        assertEquals(null,studentXMLRepository.save(student));
+    }
+
+    @Test
+    public void bva_8(){
+
+        Validator<Student> studentValidator = new StudentValidator();
+
+        studentXMLRepository = new StudentXMLRepository(studentValidator, "studenti.xml");
+
+        Student student = new Student("id1"," ",937);
+
+        assertEquals(student,studentXMLRepository.save(student));
+    }
+
+    @Test
+    public void bva_9(){
+
+        Validator<Student> studentValidator = new StudentValidator();
+
+        studentXMLRepository = new StudentXMLRepository(studentValidator, "studenti.xml");
+
+        Student student = new Student("id1","a",937);
+
+        assertEquals(student,studentXMLRepository.save(student));
+    }
+
+    @Test
+    public void bva_10(){
+
+        Validator<Student> studentValidator = new StudentValidator();
+
+        studentXMLRepository = new StudentXMLRepository(studentValidator, "studenti.xml");
+
+        Student student = new Student("id1","1",937);
+
+        assertEquals(student,studentXMLRepository.save(student));
+    }
+
+    @Test
+    public void bva_11(){
+
+        Validator<Student> studentValidator = new StudentValidator();
+
+        studentXMLRepository = new StudentXMLRepository(studentValidator, "studenti.xml");
+
+        Student student = new Student("id1","a1b2c3",937);
+
+        assertEquals(student,studentXMLRepository.save(student));
+    }
+
+    @Test
+    public void bva_12(){
+
+        Validator<Student> studentValidator = new StudentValidator();
+
+        studentXMLRepository = new StudentXMLRepository(studentValidator, "studenti.xml");
+
+        Student student = new Student("id1","z",937);
+
+        assertEquals(student,studentXMLRepository.save(student));
+    }
+
+    @Test
+    public void bva_13(){
+
+        Validator<Student> studentValidator = new StudentValidator();
+
+        studentXMLRepository = new StudentXMLRepository(studentValidator, "studenti.xml");
+
+        Student student = new Student("id1","Wayne",-1);
+
+        assertEquals(null,studentXMLRepository.save(student));
+    }
+
+    @Test
+    public void bva_14(){
+
+        Validator<Student> studentValidator = new StudentValidator();
+
+        studentXMLRepository = new StudentXMLRepository(studentValidator, "studenti.xml");
+
+        Student student = new Student("id1","Wayne",0);
+
+        assertEquals(null,studentXMLRepository.save(student));
+    }
+
+    @Test
+    public void bva_15(){
+
+        Validator<Student> studentValidator = new StudentValidator();
+
+        studentXMLRepository = new StudentXMLRepository(studentValidator, "studenti.xml");
+
+        Student student = new Student("id1","a1b2c3",1);
+
+        assertEquals(null,studentXMLRepository.save(student));
+    }
+
+    @Test
+    public void bva_16(){
+
+        Validator<Student> studentValidator = new StudentValidator();
+
+        studentXMLRepository = new StudentXMLRepository(studentValidator, "studenti.xml");
+
+        Student student = new Student("id1","Wayne", Integer.MAX_VALUE);
+
+        assertEquals(null,studentXMLRepository.save(student));
+    }
+
+    @Test
+    public void bva_17(){
+
+        Validator<Student> studentValidator = new StudentValidator();
+
+        studentXMLRepository = new StudentXMLRepository(studentValidator, "studenti.xml");
+
+        Student student = new Student("id1","Wayne",Integer.MIN_VALUE);
+
+        assertEquals(null,studentXMLRepository.save(student));
+    }
 }
